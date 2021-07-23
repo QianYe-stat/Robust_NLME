e745b23e0303d61bce24b43f831fb249088411c0
Qian Ye <yexipaopao@gmail.com>
2021-07-23 15:04:19 -0700

    linear

--- src/get_sd.R
+++ src/get_sd.R
@@ -2,6 +2,9 @@ get_sd <- function(RespLog, long.data,
                    fixedest0, dispest0, invSIGMA0,
                    Bi, B, q_split,
                    Jfixed, Jraneff){
+  
+  Yrandisp <- !is.null(RespLog$randisp.loglike)
+  
   p <- length(Jfixed)
   q <- length(Jraneff)
   q1 <- q_split[1]
@@ -12,7 +15,7 @@ get_sd <- function(RespLog, long.data,
   
   Hmat.mu <- get_Hessian(RespLog$mu.loglike, pars)
   Hmat.sigma <- get_Hessian(RespLog$sigma.loglike, pars)
-  Hmat.randisp <- get_Hessian(RespLog$randisp.loglike, pars)
+  if(Yrandisp) Hmat.randisp <- get_Hessian(RespLog$randisp.loglike, pars)
   # Hmat.ran <- get_Hessian(RespLog$ran.loglike, pars)
   
   par.val <- as.list(c(fixedest0, dispest0))
@@ -21,12 +24,14 @@ get_sd <- function(RespLog, long.data,
   
   mu.val <- get_Hvalue(Hmat.mu, p+q, long.data, par.val, B)
   sigma.val <- get_Hvalue(Hmat.sigma, p+q, data=NULL, par.val, Bi)
-  randisp.val <- get_Hvalue(Hmat.randisp, p+q, data=NULL, par.val, Bi)
+  if(Yrandisp) randisp.val <- get_Hvalue(Hmat.randisp, p+q, data=NULL, par.val, Bi)
   ran.val <- bdiag(diag(0, p), -invSIGMA0*(n), diag(0, (q-q1)))
   
   
   
-  Hval <-  as.matrix(-(mu.val+sigma.val+randisp.val+ran.val))
+  if(Yrandisp) {
+    Hval <-  as.matrix(-(mu.val+sigma.val+randisp.val+ran.val))
+  } else Hval <-  as.matrix(-(mu.val+sigma.val+ran.val))
   #Hval <-  as.matrix(-(mu.val+ran.val))
   #Hval <-  as.matrix(-(mu.val+sigma.val+randisp.val))
   covMat <- ginv(Hval)
